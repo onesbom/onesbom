eae2382e15afd04ed0a57a9bd74048f9ccc92f0b
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2023-03-15 21:46:08 -0600

    Add initial SPDX type
    To kick off the tests, this commit introduces an initial SPDX 2.3
    type to enable parsing and exporting data.
    
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

---
+++ pkg/types/spdx/v23/types.go
@@ -0,0 +1,88 @@
+// SPDX-FileCopyrightText: Copyright 2023 The OneSBOM Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package v23
+
+const (
+	Version = "SPDX-2.3"
+)
+
+type Document struct {
+	ID                   string                `json:"SPDXID"`
+	Name                 string                `json:"name"`
+	Version              string                `json:"spdxVersion"`
+	DataLicense          string                `json:"dataLicense"`
+	Namespace            string                `json:"documentNamespace"`
+	CreationInfo         CreationInfo          `json:"creationInfo"`
+	DocumentDescribes    []string              `json:"documentDescribes"`
+	Files                []File                `json:"files,omitempty"`
+	Packages             []Package             `json:"packages"`
+	Relationships        []Relationship        `json:"relationships"`
+	ExternalDocumentRefs []ExternalDocumentRef `json:"externalDocumentRefs,omitempty"`
+}
+
+type Package struct {
+	FilesAnalyzed        bool                     `json:"filesAnalyzed"`
+	ID                   string                   `json:"SPDXID"`
+	Name                 string                   `json:"name"`
+	Version              string                   `json:"versionInfo"`
+	LicenseDeclared      string                   `json:"licenseDeclared,omitempty"`
+	LicenseConcluded     string                   `json:"licenseConcluded,omitempty"`
+	Description          string                   `json:"description,omitempty"`
+	DownloadLocation     string                   `json:"downloadLocation"`
+	Originator           string                   `json:"originator,omitempty"`
+	SourceInfo           string                   `json:"sourceInfo,omitempty"`
+	CopyrightText        string                   `json:"copyrightText"`
+	PrimaryPurpose       string                   `json:"primaryPackagePurpose,omitempty"`
+	HasFiles             []string                 `json:"hasFiles,omitempty"`
+	LicenseInfoFromFiles []string                 `json:"licenseInfoFromFiles,omitempty"`
+	Checksums            []Checksum               `json:"checksums"`
+	ExternalRefs         []ExternalRef            `json:"externalRefs,omitempty"`
+	VerificationCode     *PackageVerificationCode `json:"packageVerificationCode,omitempty"`
+}
+
+type File struct {
+	ID                string     `json:"SPDXID"`
+	Name              string     `json:"fileName"`
+	CopyrightText     string     `json:"copyrightText"`
+	NoticeText        string     `json:"noticeText,omitempty"`
+	LicenseConcluded  string     `json:"licenseConcluded,omitempty"`
+	Description       string     `json:"description,omitempty"`
+	FileTypes         []string   `json:"fileTypes,omitempty"`
+	LicenseInfoInFile []string   `json:"licenseInfoInFiles,omitempty"` // List of licenses
+	Checksums         []Checksum `json:"checksums"`
+}
+
+type Relationship struct {
+	Element string `json:"spdxElementId"`
+	Type    string `json:"relationshipType"`
+	Related string `json:"relatedSpdxElement"`
+}
+
+type Checksum struct {
+	Algorithm string `json:"algorithm"`
+	Value     string `json:"checksumValue"`
+}
+
+type CreationInfo struct {
+	Created            string   `json:"created"` // Date
+	Creators           []string `json:"creators"`
+	LicenseListVersion string   `json:"licenseListVersion,omitempty"`
+}
+
+type ExternalRef struct {
+	Category string `json:"referenceCategory"`
+	Locator  string `json:"referenceLocator"`
+	Type     string `json:"referenceType"`
+}
+
+type ExternalDocumentRef struct {
+	Checksum           Checksum `json:"checksum"`
+	ExternalDocumentID string   `json:"externalDocumentId"`
+	SPDXDocument       string   `json:"spdxDocument"`
+}
+
+type PackageVerificationCode struct {
+	Value         string   `json:"packageVerificationCodeValue,omitempty"`
+	ExcludedFiles []string `json:"packageVerificationCodeExcludedFiles,omitempty"`
+}
