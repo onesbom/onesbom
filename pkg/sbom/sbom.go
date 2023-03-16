9c53f5d40fa31562390bc73e347cb6ff106407d6
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2023-03-15 21:44:55 -0600

    Initial SBOM model
    Initial model of a Software Bill of Materials.
    
    This model starts with the most minimal elements to build up from here.
    
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

---
+++ pkg/sbom/sbom.go
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: Copyright 2023 The OneSBOM Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package sbom
+
+type Document struct {
+	Metadata interface{}
+	Nodes    []Node
+}
+
+type Node interface {
+}
+
+type Package struct {
+}
+
+type File struct {
+}
+
+type RelationshipType string
+
+type Relationship struct {
+	Source *Node
+	Target *Node
+	Type   RelationshipType
+}
