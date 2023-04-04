7202d468209c5b96b34e0f93d6c3940e4630b57b
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2023-04-03 20:22:34 -0600

    Add parser implementation and default skeleton
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

--- pkg/reader/parser_implementation.go
+++ pkg/reader/parser_implementation.go
@@ -1,10 +1,30 @@
 // SPDX-FileCopyrightText: Copyright 2023 The OneSBOM Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package parser
+package reader
+
+import (
+	"io"
+
+	"github.com/onesbom/onesbom/pkg/formats"
+)
 
 type parserImplementation interface {
+	OpenDocumentFile(string) (io.Reader, error)
+	DetectFormat(*Options, io.Reader) (formats.Format, error)
+	GetFormatParser(*Options, formats.Format) (FormatParser, error)
 }
 
-type defaultParserImplementation struct {
+type defaultParserImplementation struct{}
+
+func (dpi *defaultParserImplementation) OpenDocumentFile(path string) (io.Reader, error) {
+	return nil, nil
+}
+
+func (dpi *defaultParserImplementation) DetectFormat(*Options, io.Reader) (formats.Format, error) {
+	return "", nil
+}
+
+func (dpi *defaultParserImplementation) GetFormatParser(*Options, formats.Format) (FormatParser, error) {
+	return nil, nil
 }
