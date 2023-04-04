04a42712cacec1b0e020a7741bd42c0a86149e00
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2023-04-03 20:21:04 -0600

    Add parser structure
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

--- pkg/reader/parser.go
+++ pkg/reader/parser.go
@@ -1,10 +1,16 @@
 // SPDX-FileCopyrightText: Copyright 2023 The OneSBOM Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package parser
+package reader
 
-type Options struct {
-}
+import (
+	"fmt"
+	"io"
+
+	"github.com/onesbom/onesbom/pkg/sbom"
+)
+
+type Options struct{}
 
 var defaultOptions = Options{}
 
@@ -18,4 +24,34 @@ func New() *Parser {
 		Options: defaultOptions,
 		impl:    &defaultParserImplementation{},
 	}
+}
+
+// Parser reads a file and returns an sbom.Document
+func (p *Parser) ParseFile(path string) (*sbom.Document, error) {
+	f, err := p.impl.OpenDocumentFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("opening SBOM file: %w", err)
+	}
+
+	return p.ParseReader(f)
+}
+
+// Parser returns a document from a reader
+func (p *Parser) ParseReader(f io.Reader) (*sbom.Document, error) {
+	format, err := p.impl.DetectFormat(&p.Options, f)
+	if err != nil {
+		return nil, fmt.Errorf("detecting SBOM format: %w", err)
+	}
+
+	formatParser, err := p.impl.GetFormatParser(&p.Options, format)
+	if err != nil {
+		return nil, fmt.Errorf("getting format parser: %w", err)
+	}
+
+	doc, err := formatParser.Parse(&p.Options, f)
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s document: %w", format, err)
+	}
+
+	return doc, err
 }
