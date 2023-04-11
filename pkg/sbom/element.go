418991cdca391d835f196f036c408c1798cac4df
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2023-04-10 21:51:43 -0600

    Add rest of fields to finish 2.3 compat
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

--- pkg/sbom/element.go
+++ pkg/sbom/element.go
@@ -14,6 +14,7 @@ type Element struct {
 	URL              string
 	Comment          string
 	LicenseComments  string
+	Copyright        string
 	LicenseConcluded license.Expression
 	Hashes           map[string]string
 }
--- pkg/sbom/package.go
+++ pkg/sbom/package.go
@@ -5,17 +5,32 @@ package sbom
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/onesbom/onesbom/pkg/license"
 )
 
 type Package struct {
 	Element
-	SourceInfo  string
-	License     license.Expression
-	Identifiers []Identifier
+	SourceInfo       string
+	PrimaryPurpose   string // APPLICATION | FRAMEWORK | LIBRARY | CONTAINER | OPERATING-SYSTEM | DEVICE | FIRMWARE | SOURCE | ARCHIVE | FILE | INSTALL | OTHER |
+	Version          string
+	FileName         string
+	Summary          string
+	Description      string
+	Attribution      string
+	DownloadLocation string // Location to download the package
+	URL              string // URL to get more info about the package
+	Supplier         *Person
+	Originator       *Person
+	ReleaseDate      *time.Time
+	BuiltDate        *time.Time
+	ValidUntilDate   *time.Time
+	License          license.Expression
+	Identifiers      []Identifier
 }
 
+// Sets the ID of the package
 func (p *Package) SetID(newID string) {
 	p.id = newID
 }
