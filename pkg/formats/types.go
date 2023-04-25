05f424ca5f62b3cd50c13e769ced920467c056fa
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2023-04-24 19:05:04 -0600

    Change format encodings to use more mimetype-like strings
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

--- pkg/formats/types.go
+++ pkg/formats/types.go
@@ -8,30 +8,38 @@ import "strings"
 type Format string
 
 const (
-	SPDX23TV   = Format("2.3;text/spdx+text")
-	SPDX23JSON = Format("2.3;text/spdx+json")
-	SPDX22TV   = Format("2.2;text/spdx+text")
-	SPDX22JSON = Format("2.2;text/spdx+json")
+	JSON       = "json"
+	TEXT       = "text"
+	SPDX23TV   = Format("text/spdx+text;version=2.3")
+	SPDX23JSON = Format("text/spdx+json;version=2.3")
+	SPDX22TV   = Format("text/spdx+text;version=2.2")
+	SPDX22JSON = Format("text/spdx+json;version=2.2")
+	CDX14JSON  = Format("application/vnd.cyclonedx+json;version=1.4")
+	CDX15JSON  = Format("application/vnd.cyclonedx+json;version=1.5")
 )
 
-var List = []Format{SPDX23TV, SPDX23JSON, SPDX22TV, SPDX22JSON}
+var List = []Format{SPDX23TV, SPDX23JSON, SPDX22TV, SPDX22JSON, CDX14JSON, CDX15JSON}
 
-// Returns the version
+// Version returns the version of the format
 func (f *Format) Version() string {
-	parts := strings.Split(string(*f), ";")
+	parts := strings.Split(string(*f), ";version=")
 	if len(parts) > 1 {
-		return parts[0]
+		return parts[1]
 	}
 	return ""
 }
 
 // Encoding returns the encoding used by the SBOM format
-func (f *Format) Encoding() string {
-	parts := strings.Split(string(*f), "+")
-	if len(parts) > 1 {
-		return parts[1]
+func (f Format) Encoding() string {
+	// Trim the version first
+	switch {
+	case strings.Contains(string(f), JSON):
+		return JSON
+	case strings.Contains(string(f), TEXT):
+		return TEXT
+	default:
+		return ""
 	}
-	return ""
 }
 
 // Type returns the encoding used by the SBOM format
