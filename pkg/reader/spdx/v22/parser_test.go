2595d5d4dc4e667d5a570fb43297bc261fe49cbc
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2023-04-28 18:30:01 -0600

    Add SPDX 2.2 parser
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

---
+++ pkg/reader/spdx/v22/parser_test.go
@@ -0,0 +1,36 @@
+// SPDX-FileCopyrightText: Copyright 2023 The OneSBOM Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package v22
+
+import (
+	"os"
+	"testing"
+
+	"github.com/onesbom/onesbom/pkg/license"
+	"github.com/onesbom/onesbom/pkg/reader/options"
+	"github.com/onesbom/onesbom/pkg/sbom"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParse(t *testing.T) {
+	sbomFile, err := os.Open("../../testdata/linux-x64-manifest.spdx.json")
+	require.NoError(t, err)
+	defer sbomFile.Close()
+
+	parser := Parser{}
+	doc, err := parser.Parse(&options.Options{}, sbomFile)
+	require.NoError(t, err)
+	require.Len(t, doc.Nodes, 1185, "unexpected node length (%d)", len(doc.Nodes))
+
+	require.Len(t, doc.RootElements(), 1)
+	require.Len(t, (*doc.RootElements()[0]).Relationships(), 8)
+	p, ok := (*doc.RootElements()[0]).(*sbom.Package)
+	require.True(t, ok)
+	require.Len(t, p.Identifiers, 1)
+	require.Equal(t, "pkg:oci/nginx@sha256:2ab30d6ac53580a6db8b657abf0f68d75360ff5cc1670a85acb5bd85ba1b19c0?mediaType=application%2Fvnd.docker.distribution.manifest.list.v2+json\u0026repository_url=index.docker.io%2Flibrary", p.Identifiers[0].Value)
+
+	// Test licenses
+	require.Equal(t, license.Expression("Apache-2.0"), p.LicenseConcluded)
+	require.Equal(t, license.Expression("Apache-2.0 OR MIT"), p.License)
+}
