cad0c61ebd6d9adf2fdb564af58b6c58d118e2ca
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2023-05-02 15:19:48 -0600

    Add nodelist Packages and Files methods
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

--- pkg/sbom/nodelist.go
+++ pkg/sbom/nodelist.go
@@ -4,3 +4,25 @@
 package sbom
 
 type NodeList []Node
+
+// Files returns all nodes in the nodelist which are Files.
+func (nl *NodeList) Files() []*File {
+	fileList := []*File{}
+	for _, n := range *nl {
+		if f, ok := n.(*File); ok {
+			fileList = append(fileList, f)
+		}
+	}
+	return fileList
+}
+
+// Pacakges returns all nodes in the nodelist which are Packages.
+func (nl *NodeList) Packages() []*Package {
+	packageList := []*Package{}
+	for _, n := range *nl {
+		if p, ok := n.(*Package); ok {
+			packageList = append(packageList, p)
+		}
+	}
+	return packageList
+}
