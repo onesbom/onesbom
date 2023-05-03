f6f85c01331c4d9a33002a86e029c1bee780eb6d
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2023-05-02 20:21:10 -0600

    Add method to export person to SPDX2 string
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

--- pkg/sbom/person.go
+++ pkg/sbom/person.go
@@ -3,10 +3,13 @@
 
 package sbom
 
+import "fmt"
+
 type Person struct {
 	Name        string
 	Email       string
 	URL         string
+	IsOrg       bool
 	Phone       string             // CDX
 	Contacts    []Person           // CDX
 	Identifiers []PersonIdentifier // SPDX
@@ -16,4 +19,21 @@ type PersonIdentifier struct {
 	ID      string
 	Type    string
 	Comment string
+}
+
+// ToSPDX returns a rendering of the Person object as a SPDX 2.x string
+func (p *Person) ToSPDX2() string {
+	if p.Email == "" && p.Name == "" {
+		return ""
+	}
+	s := "Person"
+	if p.IsOrg {
+		s = "Organization"
+	}
+	s = fmt.Sprintf("%s: %s", s, p.Name)
+
+	if p.Email != "" {
+		s = fmt.Sprintf("%s (%s)", s, p.Email)
+	}
+	return s
 }
