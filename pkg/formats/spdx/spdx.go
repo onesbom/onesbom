9b7c4f46dde665ec9e4d5a3ef35930e056c5808d
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2023-05-04 00:43:28 -0600

    SPDX: Add actor line parser
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

--- pkg/formats/spdx/spdx.go
+++ pkg/formats/spdx/spdx.go
@@ -3,6 +3,28 @@
 
 package spdx
 
+import "strings"
+
 const (
 	NOASSERTION = "NOASSERTION"
 )
+
+func ParseActorString(s string) (actorType, actorName, actorEmail string) {
+	s = strings.TrimSpace(s)
+	if strings.HasPrefix(s, "Person:") {
+		actorType = "person"
+		s = strings.TrimPrefix(s, "Person:")
+	} else if strings.HasPrefix(s, "Organization:") {
+		actorType = "org"
+		s = strings.TrimPrefix(s, "Organization:")
+	}
+	s = strings.TrimSpace(s)
+	actorName = s
+	if strings.HasSuffix(s, ")") && strings.Contains(s, "(") {
+		actorName = strings.TrimSpace(s[0:strings.LastIndex(s, "(")])
+		actorEmail = strings.TrimSpace(s[strings.LastIndex(s, "(")+1:])
+		actorEmail = strings.TrimSuffix(actorEmail, ")")
+	}
+
+	return actorType, actorName, actorEmail
+}
